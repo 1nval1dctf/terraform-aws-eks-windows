34e213fc8a7300ba84b6591bf9b01d2209661fb5
1nv@l1d <invalidctf@protonmail.com>
2025-03-05 07:22:43 +1100

    update external-dns image and fix service account role binding

--- modules/eks-extras/external_dns.tf
+++ modules/eks-extras/external_dns.tf
@@ -13,7 +13,8 @@ data "aws_iam_policy_document" "route53_change_records" {
   statement {
     actions = [
       "route53:ListHostedZones",
-      "route53:ListResourceRecordSets"
+      "route53:ListResourceRecordSets",
+      "route53:ListTagsForResource"
     ]
 
     resources = [
@@ -29,27 +30,60 @@ resource "aws_iam_policy" "route53_change_records" {
   policy = data.aws_iam_policy_document.route53_change_records[0].json
 }
 
-resource "aws_iam_role_policy_attachment" "linux_node_group_dns" {
+resource "aws_iam_role_policy_attachment" "external_dns" {
   count      = var.external_dns_support ? 1 : 0
   policy_arn = aws_iam_policy.route53_change_records[0].arn
-  role       = var.linux_node_group_iam_role
+  role       = aws_iam_role.external_dns[0].name
 }
 
-resource "aws_iam_role_policy_attachment" "windows_node_group_dns" {
-  count      = var.external_dns_support ? var.windows_support ? 1 : 0 : 0
-  policy_arn = aws_iam_policy.route53_change_records[0].arn
-  role       = var.windows_node_group_iam_role
+data "aws_caller_identity" "current" {}
+data "aws_eks_cluster" "selected" {
+  name = var.eks_cluster_name
+}
+data "aws_iam_policy_document" "eks_oidc_assume_role" {
+  count = var.external_dns_support ? 1 : 0
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_eks_cluster.selected.identity[0].oidc[0].issuer, "https://", "")}:sub"
+      values = [
+        "system:serviceaccount:default:external-dns"
+      ]
+    }
+    principals {
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${replace(data.aws_eks_cluster.selected.identity[0].oidc[0].issuer, "https://", "")}"
+      ]
+      type = "Federated"
+    }
+  }
 }
 
+resource "aws_iam_role" "external_dns" {
+  count       = var.external_dns_support ? 1 : 0
+  name        = "external-dns"
+  description = "Permissions required by external-dns to do it's job."
+
+
+  force_detach_policies = true
+
+  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role[0].json
+}
 #tfsec:ignore:GEN003
-resource "kubernetes_service_account_v1" "external_dns" {
+resource "kubernetes_service_account" "external_dns" {
   count    = var.external_dns_support ? 1 : 0
   provider = kubernetes
   metadata {
     name = "external-dns"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.external_dns[0].arn
+    }
   }
   automount_service_account_token = "true"
 }
+
 resource "kubernetes_cluster_role" "external_dns" {
   count = var.external_dns_support ? 1 : 0
   metadata {
@@ -96,11 +130,11 @@ resource "kubernetes_cluster_role_binding" "external_dns" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = "external-dns"
+    name      = kubernetes_service_account.external_dns[0].metadata[0].name
   }
   subject {
     kind      = "ServiceAccount"
-    name      = "external-dns"
+    name      = kubernetes_service_account.external_dns[0].metadata[0].name
     namespace = "default"
   }
 }
